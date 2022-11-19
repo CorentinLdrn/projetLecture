2bfc83578401eddf7524367a045344427136c32b
Corentin Laudrin <corentin.laudrin@gmail.com>
2022-11-19 17:15:47 -0300

    maj 19/11

--- ui/src/components/home.js
+++ ui/src/components/home.js
@@ -10,17 +10,30 @@ import "swiper/css/pagination";
 
 import { EffectCoverflow, Pagination } from "swiper";
 
+const BookInfo = () => {
+  return <h1>BookName</h1>;
+};
+
 const Home = () => {
   const [bookList, setBookList] = useState([]);
+  const [year, setYear] = useState(2022);
+  const [visible, setVisible] = useState(false);
   useEffect(() => {
     axios.get("http://localhost:5000/books").then((allBooks) => {
       setBookList(allBooks.data);
     });
   }, []);
+  const availableYears = [2020, 2021, 2022];
 
   return (
     <>
       <Link to={"/bookList"}>Liste</Link>
+      {availableYears.map((year) => (
+        <button key={year.toString()} onClick={() => setYear(year)}>
+          {year}
+        </button>
+      ))}
+
       <Swiper
         //loop={true}
         effect={"coverflow"}
@@ -39,13 +52,19 @@ const Home = () => {
         className="mySwiper"
       >
         {bookList.map((book) =>
-          book.reading === 2022 ? (
-            <SwiperSlide>
+          book.reading === year ? (
+            <SwiperSlide
+              key={book._id}
+              onClick={() => {
+                setVisible(!visible);
+              }}
+            >
               <img src={book.cover} alt="" />
             </SwiperSlide>
           ) : null
         )}
       </Swiper>
+      {visible && <BookInfo />}
     </>
   );
 };
